d72e8d62d5eba5aee935605d2b65f9194272f2a4
Musab36 <musabhd@gmail.com>
2018-01-17 01:56:39 +0300

    SectionsPagerAdapter

---
+++ app/src/main/java/com/salajim/musab/funnyjokes/SectionsPagerAdapter.java
@@ -0,0 +1,42 @@
+package com.salajim.musab.funnyjokes;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Musab on 1/17/2018.
+ */
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> mFragmentList = new ArrayList<>();//Keeps track of the fragments
+    private final List<String> mFragmentTitleList = new ArrayList<>();//Keeps track of the names of the fragments
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    //Adds the fragments to the fragment list
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+}

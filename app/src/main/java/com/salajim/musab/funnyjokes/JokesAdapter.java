6a549043d44c1d95c1aa5b61d1bca718f330d82a
Musab36 <musabhd@gmail.com>
2018-01-18 02:24:18 +0300

    RecyclerView.Adapter setup

---
+++ app/src/main/java/com/salajim/musab/funnyjokes/JokesAdapter.java
@@ -0,0 +1,60 @@
+package com.salajim.musab.funnyjokes;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Musab on 1/18/2018.
+ */
+
+public class JokesAdapter extends RecyclerView.Adapter<JokesAdapter.JokesViewHolder> {
+    private ArrayList<AddJokes> mAddJokes = new ArrayList<>();
+    private Context mContext;
+
+    public JokesAdapter(Context context, ArrayList<AddJokes> jokes) {
+        mContext = context;
+        mAddJokes = jokes;
+    }
+
+    @Override
+    public JokesAdapter.JokesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.items, parent, false);
+        JokesViewHolder viewHolder = new JokesViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(JokesAdapter.JokesViewHolder holder, int position) {
+        holder.bindJokes(mAddJokes.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mAddJokes.size();
+    }
+
+    public class JokesViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.jokesTextView) TextView mJokesTextView;
+        private Context mContext;
+
+        public JokesViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+        }
+
+        public void bindJokes(AddJokes addJokes) {
+
+            mJokesTextView.setText(addJokes.getJoke());
+        }
+    }
+}
